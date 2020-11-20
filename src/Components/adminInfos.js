85b86a431691231e047b598260917a84ee7e11a1
Beenair <b.thomas@beenair.fr>
2020-11-20 23:31:21 +0100

    Modification de la page adminInfos

--- src/Components/adminInfos.js
+++ src/Components/adminInfos.js
@@ -6,14 +6,16 @@ export default class AdminInfo extends Component {
 
 
   state = {
-      infosGenerales: [],
+      profil: [],
   }
 
+  
 
   componentDidMount() {
 
-    axios.get(`https://cv.beenair.fr/profil.php`).then(res=>{
-      this.setState({infosGenerales : res.data});
+    axios.get(`https://cv.beenair.fr/profil.php`)
+    .then(res=>{
+      this.setState({profil : res.data});
     })
   }
 
@@ -37,27 +39,27 @@ export default class AdminInfo extends Component {
 
               <div className="flex flex-col">
                 <label>Nom :</label>
-    <input type="text"className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none">{}</input>
+                <input type="text" value={this.state.profil.nom} className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
               </div>
 
               <div className="flex flex-col">
                 <label>Prenom :</label>
-                <input type="text"className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
+                <input type="text" value={this.state.profil.prenom} className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
               </div>
 
               <div className="flex flex-col">
                 <label>Date de naissance :</label>
-                <input type="text"className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
+                <input type="text" value= {this.state.profil.date_naissance} className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
               </div>
 
               <div className="flex flex-col">
                 <label>Email :</label>
-                <input type="text"className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
+                <input type="text" value= {this.state.profil.mail} className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
               </div>
 
               <div className="flex flex-col">
                 <label>Téléphone :</label>
-                <input type="text"className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
+                <input type="text" value= {this.state.profil.telephone} className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
               </div>
             
             </div>
@@ -68,23 +70,25 @@ export default class AdminInfo extends Component {
 
               <div className="flex flex-col">
                 <label>Code postale :</label>
-                <input type="text"className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
+                <input type="text" value= {this.state.profil.adresse_code_postale} className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
               </div>
             
               <div className="flex flex-col">
                 <label>Ville :</label>
-                <input type="text"className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
+                <input type="text" value= {this.state.profil.adresse_ville} className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
               </div>
 
               <div className="flex flex-col">
                 <label>Permis :</label>
-                <input type="text"className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
+                <input type="text" value={this.state.profil.permis} className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>
               </div>
 
               <div className="flex flex-col">
                 <label>Mobilité :</label>
-                <input type="text"className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>      
+                <input type="text" value={this.state.profil.mobilite} className="px-1 h-8 rounded-md border-2 focus:border-2 focus:border-blue-400 focus:outline-none"></input>      
               </div>
+
+            
 
             </div>
 
